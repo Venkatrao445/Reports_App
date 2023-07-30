4cd4b2a251abb2a7980d11be65585373e1414466
venky <venkatraogurijala3@gmail.com>
2023-07-30 09:57:18 +0530

    Adding CitizenPlan entity

---
+++ src/test/java/com/reports/ReportsApp/ReportsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reports.ReportsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
